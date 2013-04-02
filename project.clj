d906491f0377dc03059dd75b1ca621a028bdbae9
Jan Kronquist <jan.kronquist@gmail.com>
2013-04-02 09:06:56 +0200

    Fixed incorrect package name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject rock-paper-scissors "1.0.0-SNAPSHOT"
   :description "The game rock-paper-scissors implemented using CQRS & Event Sourcing in Clojure"
   :dependencies [[org.clojure/clojure "1.5.0"]]
-  :main rps.main)
+  :main com.jayway.rps.main)
